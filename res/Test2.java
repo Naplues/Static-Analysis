bbc326ede8c5bad3ecf416961df19b65dac4966f
naplues <naplues@163.com>
2017-12-24 22:03:27 +0800

    while循环中break和continue测试 Signed-off-by: naplues <naplues@163.com>

--- res/Test2.java
+++ res/Test2.java
@@ -1,17 +1,27 @@
+
 public void sample(int a, int b)
 {
-	for(int i=0;i<5;i++) {
-		if(a>0) {
-			a++;
-			continue;
-		}
-		for(;;) {
-			a++;
-			if(a==c) {
-				continue;
+	for (int i = 0; i < structure.length(); i++) {
+		if (structure.charAt(i) == '|') {
+			System.out.println(i);
+			for (int j = i - 1; j >= 0; j--) {
+				if (structure.charAt(j) == ')') {
+					stack.push(')');
+					continue;
+				}
+				if (structure.charAt(j) == '(') {
+					if (!stack.isEmpty()) {
+						stack.pop();
+						continue;
+					} else {
+						// j-1处0改为1
+						structure.setCharAt(j - 1, '1');
+						break;
+					}
+
+				}
 			}
 		}
-		a++;
 	}
 
 }
